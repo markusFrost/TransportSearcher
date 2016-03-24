fc4c249c70b1d14aabf7ad7ce14e2a08ed448763
markusFrost <andreywystawkin@yandex.ru>
2016-03-24 18:31:24 +0400

    add Station object

---
+++ TS_desktop/src/models/Station.java
@@ -0,0 +1,13 @@
+package models;
+
+public class Station {
+    private String mName;
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+}
